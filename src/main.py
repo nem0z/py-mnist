6470c023c248fb376143bee6f768e6d3735fe1c1
nem0z <maxime.bernard.dev@gmail.com>
2023-02-28 14:40:29 +0100

    refactor (main): rewrite main using dataset class

--- src/main.py
+++ src/main.py
@@ -1,26 +1,23 @@
 import gzip
 import numpy as np
 import matplotlib.pyplot as plt
-from utils.parse import parse_label, parse_image
+
+from dataset import Dataset
 
 DATASETS_PATH = "./datasets/"
 TRAIN_LABEL = "train-labels-idx1-ubyte.gz"
 TRAIN_IMAGE = "train-images-idx3-ubyte.gz"
 T10K_LABEL = "t10k-labels-idx1-ubyte.gz"
 T10K_IMAGE = "t10k-images-idx3-ubyte.gz"
+
+train_dataset = Dataset()
+t10k_dataset = Dataset()
     
-train_lables = parse_label(DATASETS_PATH + TRAIN_LABEL)
-train_images = parse_image(DATASETS_PATH + TRAIN_IMAGE)
-
-labels = parse_label(DATASETS_PATH + T10K_LABEL)
-images = parse_image(DATASETS_PATH + T10K_IMAGE)
+train_dataset.load(DATASETS_PATH+TRAIN_LABEL, DATASETS_PATH+TRAIN_IMAGE)
+t10k_dataset.load(DATASETS_PATH+T10K_LABEL, DATASETS_PATH+T10K_IMAGE)
     
-    
-print(len(train_images), len(train_lables))
-print(len(images), len(labels))
+train_dataset.plot_labels()
+t10k_dataset.plot_labels()
 
-unique, counts = np.unique(train_lables, return_counts=True)
+input("Press [enter] to quit...")
 
-plt.bar(unique, counts)
-
-plt.show()
